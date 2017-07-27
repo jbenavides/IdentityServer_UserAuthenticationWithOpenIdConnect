eff2f0e21055339cdb64b830e3a215f82e061bfc
Jose Benavides <jbenavides.bermudez@gmail.com>
2017-07-27 12:34:16 -0500

    adding example code

---
+++ MvcClient/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page - Secure page.";
+
+            return View();
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.Authentication.SignOutAsync("Cookies");
+            await HttpContext.Authentication.SignOutAsync("oidc");
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
